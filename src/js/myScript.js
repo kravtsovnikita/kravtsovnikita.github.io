039ae604269177a76b759e0a074884bf960ebdf3
kravtsovnikita <96532057+kravtsovnikita@users.noreply.github.com>
2022-01-26 15:50:45 +0300

    troubles with 6th homework №2

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -82,20 +82,8 @@ function onEntry (entry){
 
 
 // Якорные ссылки
-$(window).scroll(() >= {
-	let scrollDistance = $(window).scrollTop();
-	$(".section").each(i,el) >= {
-		if($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
-	    $("nav a").each(i,el) >= {
-	    	if($(el).hasClass("active")){
-	    		$(el).removeClass("active");
-	    	}
-	    }
-		}
-	}
-})
 
-$('nav li:eq('+ i +')').find('a').addClass('active');
+
 
 
 
